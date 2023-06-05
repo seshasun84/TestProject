4c16316bc51d45a9f65f760dff78629958fec962
seshasun84 <seshasun84@gmail.com>
2023-06-05 11:05:29 -0500

    my first commit

---
+++ src/main/java/com/test/TestProject/TestProject/Application.java
@@ -0,0 +1,9 @@
+package com.test.TestProject.TestProject;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello TestProject");
+    }
+
+}
---
+++ src/test/java/com/test/TestProject/Dictionary.java
@@ -0,0 +1,10 @@
+package com.test.TestProject;
+
+public class Dictionary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
